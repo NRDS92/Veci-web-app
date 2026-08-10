614bf26450466f93005c4f3992a4a1c205d7c0dc
Your Name <you@example.com>
2026-08-10 09:56:25 +0200

    migration(admin): admin migrated to another project

--- src/app/admin/dashboard/page.tsx
+++
@@ -1,5 +0,0 @@
-import Dashboard from "@/components/admin/Dashboard/Dashboard";
-
-export default function DashboardPage() {
-    return <Dashboard />;
-}
--- src/features/admin/services/admin.service.ts
+++
@@ -1,2 +0,0 @@
-export * from "../events/services/events.admin.service";
-export * from "../business/services/business.admin.service";
